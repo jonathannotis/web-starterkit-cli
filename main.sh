b4cdd96312f0ed380d2b15212524ed17d9c4971e
Jonathan Notis <jrnotis@gmail.com>
2022-08-23 20:05:34 -0400

    first commit

---
+++ main.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+# https://www.notion.so/josephnotis/Web-Starterkit-CLI-10a7fcef03374966a393e5ecc82cc6bd
+
+# <command name> <frontend> <backend> <appname> -p <packages> --typescript --yarn
