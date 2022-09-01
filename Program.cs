963dfd8ced92fd30b5ab2cab55b051679fcf87e3
Joseph Notis <jnotis99@gmail.com>
2022-09-01 19:02:07 -0400

    Added .net scaffold

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WebStarterkit
+{
+  public class WebStarterkit
+  {
+    public static void Main(string[] args)
+    {
+      // TODO: Call function that reads in and delegates work for args
+      System.Console.WriteLine("Hello world!");
+      Array.ForEach(args, arg => Console.WriteLine(arg));
+    }
+  }
+}
