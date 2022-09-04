387fbb6c4994bd0cf98408ea8b0e065b16b54330
Jonathan Notis <jrnotis@gmail.com>
2022-09-04 18:19:43 -0400

    separated args into variables

--- Program.cs
+++ Program.cs
@@ -1,14 +1,47 @@
-﻿using System;
+﻿using System.Text.RegularExpressions;
+
 
 namespace WebStarterkit
 {
-  public class WebStarterkit
-  {
-    public static void Main(string[] args)
+    public class WebStarterkit
     {
-      // TODO: Call function that reads in and delegates work for args
-      System.Console.WriteLine("Hello world!");
-      Array.ForEach(args, arg => Console.WriteLine(arg));
+        public static void Main(string[] args)
+        {
+
+            // <command name> <frontend> <backend> <appname> -pkg <packages> --typescript --yarn
+
+            string frontend = args[0];
+            string backend = args[1];
+            string directoryName = args[2];
+
+            bool typescript = args.Contains("--typescript"); // typescript/javascript
+            bool yarn = args.Contains("--yarn"); // yarn/npm
+
+
+            List<string>? packages = args[3].Equals("-pkg") ? GetPackages(args) : null;
+            packages?.ForEach(package =>
+            {
+                Console.WriteLine(package); // prints out each package
+
+            });
+
+            // Regex for dash then letter is -[a-zA-z]
+
+        }
+
+        private static List<string> GetPackages(string[] args)
+        {
+            List<string> packages = new List<string>();
+            for (int i = 4; i < args.Length; i++)
+            {
+                if (args[i].Contains("--"))
+                {
+                    break;
+                }
+                packages.Add(args[i]);
+            }
+
+            return packages;
+        }
     }
-  }
 }
